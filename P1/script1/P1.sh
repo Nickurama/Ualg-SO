ee51ee4f157f954289d9164bcc332da1fc4bca64
Nickurama <rokdoombeast@gmail.com>
2024-02-07 15:25:21 +0000

    P1

---
+++ P1/script1/P1.sh
@@ -0,0 +1,15 @@
+mkdir dir1
+cd dir1
+echo "Diogo" > name.txt
+cd ..
+
+mkdir dir2
+cd dir2
+echo "Fonseca" > surename.txt
+cd ..
+
+mkdir dir3
+cd dir3
+echo "03-01-2003" > birthday.txt
+cd ..
+
---
+++ P1/script2/script2.sh
@@ -0,0 +1,2 @@
+read -p "extension: " uservar
+ls *$uservar
